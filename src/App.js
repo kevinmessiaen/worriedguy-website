ecd295940f3d3696708057cbf4cd1f5cfe10178d
Kevin Messiaen <kevin.messiaen@icloud.com>
2024-12-01 09:12:37 +0100

    Improved button hover color

--- src/App.js
+++ src/App.js
@@ -1,50 +1,10 @@
 import logo from './logo.webp';
-import X from "./components/icons/X";
-import Telegram from "./components/icons/Telegram";
-import Instagram from "./components/icons/Instagram";
-import TikTok from "./components/icons/TikTok";
-import WorriedGuy from "./components/icons/WorriedGuy";
-import DexScreener from "./components/icons/DexScreener";
-import Radyum from "./components/icons/Radyum";
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <div>
-      <header className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 font-gluten">
-         <div className="flex-1" />
-         <ul className="font-bold text-xl flex p-0 space-x-2 rtl:space-x-reverse mt-0">
-           <li>
-             <a href="https://t.co/iphzwaYmqv" target="_blank" rel="noreferrer" className="block py-2 px-3 text-black rounded hover:bg-amber-100">
-               <Telegram />
-             </a>
-           </li>
-            <li>
-             <a href="https://x.com/WorriedGuyCTO1" target="_blank" rel="noreferrer" className="block py-2 px-3 text-black rounded hover:bg-amber-100">
-               <X />
-             </a>
-           </li>
-           <li>
-             <a href="https://www.instagram.com/worriedguy.meme?igsh=MWg2YWRtcXUxaTRnYw%3D%3D&utm_source=qr" target="_blank" rel="noreferrer" className="block py-2 px-3 text-black rounded hover:bg-amber-100">
-               <Instagram />
-             </a>
-           </li>
-           <li>
-            <a href="https://www.tiktok.com/@worriedguy100?_t=8ra3lXhS0wb&_r=1" target="_blank" rel="noreferrer" className="block py-2 px-3 text-black rounded hover:bg-amber-100">
-              <TikTok />
-            </a>
-          </li>
-          <li>
-            <a href="https://dexscreener.com/solana/9mylvrcbxzgmfjgab4t7b7pg3jxbyd6j9utadwgxqhfj" target="_blank" rel="noreferrer" className="block py-2 px-3 text-black rounded hover:bg-amber-100">
-              <DexScreener />
-            </a>
-          </li>
-          <li>
-            <a href="https://raydium.io/swap/?inputMint=sol&outputMint=DT3zuw4qzDw2Fp6Uh5W1aMZBK2wfGWZWjJUfJY9Qpump" target="_blank" rel="noreferrer" className="block py-2 px-3 text-black rounded hover:bg-amber-100">
-              <Radyum />
-            </a>
-          </li>
-         </ul>
-      </header>
+      <Header />
       <main className="container mx-auto flex flex-col items-center">
         <img src={logo} alt="Worried Guy selfie" className="max-w-full w-[32rem]" />
         <h1 className="uppercase text-4xl font-bold font-chelsea-market text-center">
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,22 @@ module.exports = {
       fontFamily :{
         'chelsea-market': ["chelsea-market", "sans-serif"],
         gluten: ["gluten", "sans-serif"],
+      },
+      colors: {
+          'granite-green': {
+                  '50': '#f4f3f1',
+                  '100': '#e7e5e0',
+                  '200': '#d1cec5',
+                  '300': '#b4b1a2',
+                  '400': '#8c8773',
+                  '500': '#7c7866',
+                  '600': '#615e4f',
+                  '700': '#4c4a3f',
+                  '800': '#3f3d35',
+                  '900': '#373630',
+                  '950': '#1c1b17',
+              },
+
       }
     },
   },
